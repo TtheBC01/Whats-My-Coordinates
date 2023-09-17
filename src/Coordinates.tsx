e0ae7cb2d4bc02ae0c285771180091bb4459c966
TtheBC01 <tac688@alumni.stanford.edu>
2023-09-17 11:40:08 -0700

    added toast

--- src/Coordinates.tsx
+++ src/Coordinates.tsx
@@ -1,34 +1,67 @@
 import { useState } from "react";
+import toast, { Toaster } from "react-hot-toast";
 
 export default function Coordinates() {
 
     const [latitude, setLatitude] = useState<number | null>(null);
     const [longitude, setLongitude] = useState<number | null>(null);
+    const [accuracy, setAccuracy] = useState<number | null>(null);
+    const [altitude, setAltitude] = useState<number | null>(null);
+    const [altitudeAccuracy, setAltitudeAccuracy] = useState<number | null>(null);
+    
 
     function handleOnClick() {
         if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(showPosition);
+            navigator.geolocation.getCurrentPosition(showPosition, showError);
         } else {
-            console.log("Error: Geolocation not available")
+            toast.error('Location functionality not available, check your privacy settings.')
         }
     }
 
     function showPosition(position: GeolocationPosition) {
         setLatitude(position.coords.latitude);
         setLongitude(position.coords.longitude);
+        setAccuracy(position.coords.accuracy);
+        setAltitude(position.coords.altitude);
+        setAltitudeAccuracy(position.coords.altitudeAccuracy);
+        toast.success('Location determined!')
     }
+
+    function showError(error: GeolocationPositionError) {
+        switch(error.code) {
+          case error.PERMISSION_DENIED:
+            toast.error("User denied the request for Geolocation.")
+            break;
+          case error.POSITION_UNAVAILABLE:
+            toast.error("Location information is unavailable.")
+            break;
+          case error.TIMEOUT:
+            toast.error("The request to get user location timed out.")
+            break;
+        }
+      }
 
     return (
         <>
             <button onClick={handleOnClick}>
                 What's my Coordinates?
             </button>
+            <Toaster />
             <div>
                 <p>
                     Latitude: {latitude}
                 </p>
                 <p>
                     Longitude: {longitude}
+                </p>
+                <p>
+                    Accuracy: {accuracy}
+                </p>
+                <p>
+                    Altitude (meters): {altitude}
+                </p>
+                <p>
+                    Altitude Accuracy (meter): {altitudeAccuracy}
                 </p>
             </div>
         </>
