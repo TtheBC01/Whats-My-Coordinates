6b7644910fd487050f2941e3b94d0042cdafcab8
TtheBC01 <tac688@alumni.stanford.edu>
2023-09-24 21:59:49 -0700

    added github link

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import { TfiGithub } from 'react-icons/tfi';
 import Coordinates from './Coordinates';
 
 function App() {
@@ -8,6 +9,12 @@ function App() {
       <header className="App-header">
         <Coordinates />
       </header>
+      <div className="social-container">
+        <a href="https://github.com/TtheBC01/Whats-My-Coordinates"
+          className="github">
+          <TfiGithub />
+        </a>
+      </div>
     </div>
   );
 }
